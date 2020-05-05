304cbb6cc6510cbb57011863ebdb2ac5dfda33d9
Shubham Wawale <wawaleshubham@gmail.com>
2020-05-05 15:23:03 +0530

    add a header file for spaceship

---
+++ spaceship.h
@@ -0,0 +1,18 @@
+#define MAX_SPACESHIP_SPEED 20
+#define SPACESHIP_BRAKE_GRADIENT 1.2
+#define SPACESHIP_ACC_GRADIENT 1.2
+#define MAX_TURN_RATE DEG_TO_RAD(6)
+
+extern ALLEGRO_BITMAP *SPRITESHEET, *SHIP;
+
+typedef struct{
+    float x, y, speed, heading, scale;
+    bool is_drifting;
+}SPACESHIP;
+
+extern SPACESHIP *ship;
+extern ALLEGRO_TRANSFORM ship_transform;
+void init_ship();
+void draw_ship(SPACESHIP *ship);
+void ship_update(SPACESHIP *ship);
+
