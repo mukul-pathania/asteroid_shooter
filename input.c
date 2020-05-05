14df218cf4b13c32a7aba28c310b1e7a10af4f8a
Mukul Pathania <mukulpathania96@gmail.com>
2020-05-05 15:30:54 +0530

    Add files for handling keyboard state and events

---
+++ input.c
@@ -0,0 +1,24 @@
+#include "input.h"
+    
+bool key[ALLEGRO_KEY_MAX];
+
+void init_input()
+{
+    memset(key, false, sizeof(key));
+}
+
+void keyboard_update(ALLEGRO_EVENT* event)
+{
+    switch(event->type)
+    {
+
+        case ALLEGRO_EVENT_KEY_DOWN:
+            key[event->keyboard.keycode] = true;
+            break;
+        case ALLEGRO_EVENT_KEY_UP:
+            key[event->keyboard.keycode] = false;
+            break;
+    }
+}
+
+
---
+++ input.h
@@ -0,0 +1,7 @@
+#include <allegro5/allegro5.h>
+
+extern bool key[ALLEGRO_KEY_MAX];
+void init_input();
+void keyboard_update(ALLEGRO_EVENT* event);
+
+
