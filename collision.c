2d0d9aae980a05b726b3a26a997e74c62b8ebc51
Mukul Pathania <mukulpathania96@gmail.com>
2020-05-10 15:33:03 +0530

    Modify collsion detection function to return the number of collisons

--- collision.c
+++ collision.c
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <allegro5/allegro5.h>
 
+/*Two circles are provided to this function and it returns true if they collide and false otherwise.
+ */
 bool is_colliding(BOUNDING_CIRCLE* circle1, BOUNDING_CIRCLE* circle2){
     float dx, dy, distance;
     dx = circle1->x - circle2->x;
@@ -15,23 +17,26 @@ bool is_colliding(BOUNDING_CIRCLE* circle1, BOUNDING_CIRCLE* circle2){
     return false;
 }
 
-bool check_and_handle_collisions(){
+/*This function handles the collisions between the asteroids and the blasts and returns the number 
+ *of collsions and 0 if no collisions took place.
+ */
+int check_and_handle_collisions(){
+    int num_collisions = 0;
     for(int i = 0; i < MAX_BLASTS_ON_SCREEN; i++){
-        if(blasts[i].gone)
+        if(blasts[i].gone)//condition is true if blast is not on screen
             continue;
 
         for(int j = 0; j < MAX_ASTEROID_COUNT; j++){
-            if(asteroids[j].gone)
+            if(asteroids[j].gone)//condition is true if the asteroid is not on screen
                 continue;
             if(is_colliding(&blasts[i].circle, &asteroids[j].circle)){
                 asteroids[j].life--;
                 blasts[i].gone = true;
                 blasts_on_screen--;
-                return true;
             }
         }
     }
-    return false;
+    return num_collisions;
 }
                 
             
